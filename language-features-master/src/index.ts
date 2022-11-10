6f99a873989784e2501b0eb939e056256be3adc3
VLSLV333 <v.sherstyk@gmail.com>
2022-11-10 12:10:44 +0000

    trying to work

---
+++ language-features-master/src/index.ts
@@ -0,0 +1,25 @@
+/*
+* Task 1: Times
+*/
+function times(array, times) {
+    // implement your solution here
+    throw new Error('Not implemented');
+}
+
+/*
+* Task 2: Logger
+*/
+function logger(data, serviceInfo) {
+    // implement your solution here
+    throw new Error('Not implemented');
+}
+
+/*
+* Task 3: Array NTH Element
+*/
+function everyNth(arr, nth) {
+    // implement your solution here
+    throw new Error('Not implemented');
+}
+
+export {times, logger, everyNth}
